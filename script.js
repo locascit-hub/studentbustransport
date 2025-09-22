289a62e0225308e01f34a48337ea8aa834a2b2e6
ezhil srikanth <mr.srik307@gmail.com>
2025-09-22 12:42:42 +0530

    dev url added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const bNo=new URLSearchParams(window.location.search).get('bNo');
 const iframe=document.querySelector('iframe');
 if(bNo){
-  iframe.src=`https://cit-locas-transport-dev-locascit-hubs-projects.vercel.app/route-detail/${bNo}`;
+  iframe.src=`https://dev-cit-locas-transport.vercel.app/route-detail/${bNo}`;
 }
 else{
-  iframe.src=`https://cit-locas-transport-dev-locascit-hubs-projects.vercel.app/route-detail/63`;
+  iframe.src=`https://dev-cit-locas-transport.vercel.app/route-detail/63`;
 }
