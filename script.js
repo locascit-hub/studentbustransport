f7ac0c197f57dbc466012a0ad30733c76568eee2
locascit-hub <locascit@gmail.com>
2025-09-22 11:09:33 +0530

    Update script.js for dev

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const bNo=new URLSearchParams(window.location.search).get('bNo');
 const iframe=document.querySelector('iframe');
 if(bNo){
-  iframe.src=`http://10.101.66.88:5000/route-detail/${bNo}`;
+  iframe.src=`https://cit-locas-transport-dev-locascit-hubs-projects.vercel.app/route-detail/${bNo}`;
 }
 else{
-  iframe.src=`http://10.101.66.88:5000/route-detail/63`;
+  iframe.src=`https://cit-locas-transport-dev-locascit-hubs-projects.vercel.app/route-detail/63`;
 }
